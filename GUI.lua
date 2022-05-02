3db2fc9bb8c3167e012bc0f779d98f913cb106e0
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-05-02 21:09:07 +0100

    fix

--- GUI.lua
+++ GUI.lua
@@ -19,7 +19,7 @@ local Themes = {
     Accent = Color3.fromRGB(10, 10, 10),
     LightContrast = Color3.fromRGB(20, 20, 20),
     DarkContrast = Color3.fromRGB(14, 14, 14),
-    TextColour = Color3.fromRGB(255, 255, 255)
+    TextColor = Color3.fromRGB(255, 255, 255)
 }
 
 -- // Aiming Page
