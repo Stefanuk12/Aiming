e0511d426404e1b3d2d9a43727994e8e783fd811
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-05-02 23:21:41 +0100

    Added GUI support to AimLock

--- Examples/AimLock.lua
+++ Examples/AimLock.lua
@@ -59,5 +59,120 @@ RunService:BindToRenderStep("AimLockAiming", 0, function()
     end
 end)
 
+-- // Check if GUI exists
+if (Aiming.GUI) then
+    -- // Vars
+    local UI = Aiming.GUI[1]
+
+    -- //
+    local AimLockPage = UI:addPage({
+        title = "Aim Lock",
+        icon = 5012544693
+    })
+
+    -- //
+    local MainSection = AimLockPage:addSection({
+        title = "Main"
+    })
+
+    MainSection:addToggle({
+        title = "Enabled",
+        default = Settings.Enabled,
+        callback = function(value)
+            Settings.Enabled = value
+        end
+    })
+
+    MainSection:addKeybind({
+        title = "Keybind",
+        default = Settings.Keybind,
+        changedCallback = function(value)
+            Settings.Keybind = value
+        end
+    })
+
+    -- //
+    local BeizerA = AimLockPage:addSection({
+        title = "Beizer A: Mouse"
+    })
+
+    BeizerA:addSlider({
+        title = "Smoothness",
+        min = 0,
+        max = 1,
+        precision = 4,
+        default = BeizerCurve.ManagerA.Smoothness,
+        callback = function(value)
+            BeizerCurve.ManagerA.Smoothness = value
+        end
+    })
+
+    BeizerA:addToggle({
+        title = "Draw Path",
+        default = BeizerCurve.ManagerA.DrawPath,
+        callback = function(value)
+            BeizerCurve.ManagerA.DrawPath = value
+        end
+    })
+
+    local function AddCurvePointSliders(Section, ManagerName)
+        -- // Vars
+        local CurvePoints = BeizerCurve["Manager" .. ManagerName].CurvePoints
+
+        -- //
+        local function AddSliderXY(i)
+            -- // Vars
+            local PointName = "Point " .. (i == 1 and "A" or "B")
+
+            -- // X Slider
+            Section:addSlider({
+                title = PointName .. ": X",
+                min = 0,
+                max = 1,
+                precision = 2,
+                default = CurvePoints[i].X,
+                callback = function(value)
+                    CurvePoints[i] = Vector2.new(value, CurvePoints[i].Y)
+                end
+            })
+
+            -- // Y Slider
+            Section:addSlider({
+                title = PointName .. ": Y",
+                min = 0,
+                max = 1,
+                precision = 2,
+                default = CurvePoints[i].Y,
+                callback = function(value)
+                    CurvePoints[i] = Vector2.new(CurvePoints[i].X, value)
+                end
+            })
+        end
+
+        AddSliderXY(1)
+        AddSliderXY(2)
+    end
+
+    AddCurvePointSliders(BeizerA, "A")
+
+    -- //
+    local BeizerB = AimLockPage:addSection({
+        title = "Beizer B: Camera"
+    })
+
+    BeizerB:addSlider({
+        title = "Smoothness",
+        min = 0,
+        max = 1,
+        precision = 4,
+        default = BeizerCurve.ManagerB.Smoothness,
+        callback = function(value)
+            BeizerCurve.ManagerB.Smoothness = value
+        end
+    })
+
+    AddCurvePointSliders(BeizerB, "B")
+end
+
 -- //
 return Aiming
