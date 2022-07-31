2b80fc8d87fc3671301f0acd5783c1b73451adc3
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-07-31 18:16:17 +0100

    Added Hood Customs

---
+++ GamePatches/Module/9825515356.lua
@@ -0,0 +1,14 @@
+--[[
+    Information:
+
+    - Hood Customs (https://www.roblox.com/games/9825515356/)
+]]
+
+-- // Dependencies
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/Module.lua"))()
+
+-- // Disable Team Check
+Aiming.Settings.Ignored.IgnoreLocalTeam = false
+
+-- // Return
+return Aiming
