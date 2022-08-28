0aa0c1380372bae1d79915b1f4156005d5c27b59
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-08-28 17:22:30 +0100

    Added partial wallback support (not all work for some reason)

--- GamePatches/Module/RushPoint.lua
+++ GamePatches/Module/RushPoint.lua
@@ -17,6 +17,8 @@ local CurrentCamera = Workspace.CurrentCamera
 local LocalPlayer = Players.LocalPlayer
 local MapFolder = Workspace.MapFolder
 local GameStats = MapFolder.GameStats
+local GetBulletIgnoreTable = require(ReplicatedStorage.Modules.Shared.GetBulletIgnoreTable)
+local ClassifyMaterial = require(ReplicatedStorage.Modules.Shared.ClassifyMaterial)
 local Characters
 
 -- // bruh. syn v2 bugging and returning empty getsenv???
@@ -30,6 +32,50 @@ local AimingIgnored = Aiming.Ignored
 local AimingSettings = Aiming.Settings
 local AimingUtilities = Aiming.Utilities
 AimingSettings.TargetPart = {"HumanoidRootPart", "Head", "LowerTorso", "UpperTorso"}
+AimingSettings.WallbangParts = {}
+AimingSettings.Wallbang = false
+
+-- // Calculates which parts can be wallbanged through
+function AimingUtilities.CalculateWallbang(ShouldSet)
+    -- // Make sure is enabled
+    if (not AimingSettings.Wallbang) then
+        return {}
+    end
+
+    -- // Vars
+    local WallbangParts = {}
+
+    -- // Loop through the entire map
+    for _, v in ipairs(MapFolder:GetDescendants()) do
+        -- // Check if the material matches
+        if (v:IsA("BasePart") and ClassifyMaterial(v)) then
+            -- // Add it
+            table.insert(WallbangParts, v)
+        end
+    end
+
+    -- // Set
+    if (ShouldSet) then
+        AimingSettings.WallbangParts = WallbangParts
+    end
+
+    -- // Return
+    return WallbangParts
+end
+AimingUtilities.CalculateWallbang(true)
+
+-- // See when the map changes, then recalculate the wallbang parts
+local function RecalculateOnChange(child)
+    -- // Ignore player stuff
+    if not (child:IsA("BasePart") and ClassifyMaterial(child)) then
+        return
+    end
+
+    -- // Recalculate
+    AimingUtilities.CalculateWallbang(true)
+end
+MapFolder.DescendantAdded:Connect(RecalculateOnChange)
+MapFolder.DescendantRemoving:Connect(RecalculateOnChange)
 
 -- // Character
 function AimingUtilities.Character(Player)
@@ -56,7 +102,7 @@ end
 -- // Ignore
 function AimingSettings.RaycastIgnore()
     local Character = AimingUtilities.Character(LocalPlayer)
-    local Ignored = {Character, CurrentCamera, Workspace.RaycastIgnore, Workspace.DroppedWeapons, MapFolder.Map.Ramps, MapFolder.Map.Walls.MapWalls}
+    local Ignored = {Character, CurrentCamera, Workspace.RaycastIgnore, Workspace.DroppedWeapons, MapFolder.Map.Ramps, MapFolder.Map.Walls.MapWalls, unpack(GetBulletIgnoreTable()), unpack(AimingSettings.WallbangParts)}
 
     return Ignored
 end
