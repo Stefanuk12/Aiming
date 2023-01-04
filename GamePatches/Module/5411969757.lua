b590a4bc6542d52d9ffdfb9e6ac47e0e55e0919c
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-01-04 12:31:10 +0000

    Added support for VIP servers

---
+++ GamePatches/Module/5411969757.lua
@@ -0,0 +1,9 @@
+--[[
+    Information:
+
+    - Futuretops: Rework (https://www.roblox.com/games/5169051062/)
+    - This is for the VIP servers
+]]
+
+-- // Run other one
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/GamePatches/Module/5169051062.lua"))()
