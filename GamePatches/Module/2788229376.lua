77e205ed2e69b8b79119939fced5baa807c33ea6
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-01-26 20:57:50 +0000

    Removed useless code

--- GamePatches/Module/2788229376.lua
+++ GamePatches/Module/2788229376.lua
@@ -8,11 +8,7 @@
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/Module.lua"))()
 
 -- // Disable Team Check
-local AimingIgnored = Aiming.Ignored
-AimingIgnored.TeamCheck(false)
-
-local AimingSettings = Aiming.Settings
-AimingSettings.Ignored.IgnoreLocalTeam = false
+Aiming.Settings.Ignored.IgnoreLocalTeam = false
 
 -- // Downed Check
 local AimingChecks = Aiming.Checks
